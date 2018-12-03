bc6abc3bc3afce037fc5af4f1a3635eb2c2f4455
Cole <swanscol@oregonstate.edu>
2018-12-02 19:28:23 -0800

    Mostly visual stuff for the cards and created the win modal

--- public/index.js
+++ public/index.js
@@ -4,16 +4,36 @@ var numCards = 4;
 var matchCounter = 0; //How many card pairs have been matched
 var flippedArray = []; //the cards during each reset period that were flipped so far
 var cardCounter  = 0;  //counts the number of cards turned before a reset
+var running = 0; //running makes sure the FlipCard function is only run once the previous one finishes(otherwise it can result in issues with the enlarge/darkening)
 
 //Copied sleep function from flaviocopes.com
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-function checkPair() {
+//for await to work, the funciton must be marked as asyncronous
+async function checkPair() {
     var check = flippedArray[0].getAttribute('data_post_id');
     for(var i = 0; i < numFlips; i++) {
         if (check != flippedArray[i].getAttribute('data_post_id')) {//check if any ids don't match
+
+            //Card enlarge visual effects
+            for(var j = 0; j < numFlips; j++){
+                flippedArray[j].classList.toggle('enlarge1');
+            }
+            //await causes the funciton to wait for the sleep
+            await sleep(300).then(() => {
+                for(var j = 0; j < numFlips; j++){
+                    flippedArray[j].classList.toggle('enlarge1');
+                    flippedArray[j].classList.toggle('enlarge2');
+                }
+            });
+            await sleep(600).then(() => {
+                for(var j = 0; j < numFlips; j++){
+                    flippedArray[j].classList.toggle('enlarge2');
+                }
+            });
+            //end visual effects
             for (var j = 0; j < numFlips; j++) {//flip cards back over if not
                 flippedArray[0].classList.toggle('flipped');
                 flippedArray[0].children[0].children[0].src = "Cardback.jpg";
@@ -22,53 +42,63 @@ function checkPair() {
             return;
         }
     }
+    //Darken flipped cards
+    for(var i = 0; i < numFlips; i++){
+        flippedArray[i].classList.toggle('darkened'); 
+    }
     matchCounter++;
     for (var i = 0; i < numFlips; i++) {
         flippedArray.shift();
     }
 }
 
-function FlipCard(event){
-    el = event.target;
-    //make sure the element isn't the card container
-    if(el.classList.contains("cardContainer") === true){
-        return;
-    }
-    //Make sure the element is the card, not the image
-    if(el.classList.contains("card") === false){
-        while(el.classList.contains("card") === false){
-            el = el.parentElement;
-        };
-    }
-    //make sure card is not flipped
-    if (el.classList.contains("flipped") === true) {
-        return;
-    }
+async function FlipCard(event){
+    if(parseInt(running) === 0){ 
+        running = 1;
+        el = event.target;
+        //make sure the element isn't the card container
+        if(el.classList.contains("cardContainer") === true){
+            running = 0;
+            return;
+        }
+        //Make sure the element is the card, not the image
+        if(el.classList.contains("card") === false){
+            while(el.classList.contains("card") === false){
+                el = el.parentElement;
+            };
+        }
+        //make sure card is not flipped
+        if (el.classList.contains("flipped") === true) {
+            running = 0;
+            return;
+        }
 
-    image = el.children[0].children[0];
+        image = el.children[0].children[0];
 
-    //If face up, flip card face down
-    if(image.src === "http://localhost:3000/" + el.getAttribute('data_url')){
-        image.src = "Cardback.jpg";
+        //If face up, flip card face down
+        if(image.src === "http://localhost:3000/" + el.getAttribute('data_url')){
+            image.src = "Cardback.jpg";
+        }
+        //If face down, flip card face up
+        else{
+            image.src = el.getAttribute('data_url');
+        }
+        el.classList.toggle("flipped");
+        cardCounter++;
+        flippedArray.push(el);
+        
+        if (cardCounter%numFlips === 0) {
+            await sleep(700).then(() => {
+                checkPair();
+            });
+        }   
+        if (matchCounter >= numCards) {
+            modal = document.getElementById('win_modal');
+            modal.classList.toggle('hidden');
+        }
     }
-    //If face down, flip card face up
-    else{
-        image.src = el.getAttribute('data_url');
-    }
-    el.classList.toggle("flipped");
-    cardCounter++;
-    flippedArray.push(el);
-    
-    if (cardCounter%numFlips === 0) {
-        sleep(700).then(() => {
-            checkPair();
-        });
-    }   
-    if (matchCounter >= numCards) {
-        //display congratulations play again modal
-    }
+    running = 0;
 }
-
 window.addEventListener('DOMContentLoaded', function () {
     var posts = document.getElementById("cardContainer");
     if(posts){
